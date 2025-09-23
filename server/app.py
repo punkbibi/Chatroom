16e9235911edc8d9283214201c03edd857228186
punkbibi <alexandre.bibilashvili.1@btu.edu.ge>
2025-09-24 00:40:58 +0400

    added FlaskSocketIO

--- server/app.py
+++ server/app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+    socketio.send("A new user joined!")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected")
+    socketio.send("A user has left the chat")
+
+
+@socketio.on('message')
+def handle_message(message):
+    socketio.send(message)
+    print(f"Message received: {message}")
+
+
+if __name__ == "__main__":
+    socketio.run(app)
